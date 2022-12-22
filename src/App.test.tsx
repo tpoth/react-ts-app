d5d387bb06bf65037f3693efe0f847cc9ab3a946
Thomas Poth <thomas.poth@pubt.de>
2022-12-22 18:06:48 +0100

    First commit

---
+++ src/App.test.tsx
@@ -0,0 +1,31 @@
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import { App, WrappedApp } from './App';
+
+describe('App', () => {
+  it('Renders hello world', () => {
+    // Arrange
+    render(<WrappedApp />);
+    // Act
+    // Expect
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Hello World');
+  });
+  it('Renders not found if invalid path', () => {
+    render(
+      <MemoryRouter initialEntries={['/this-route-does-not-exist']}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('404 - Page Not Found!');
+  });
+});
---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+export function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+export function WrappedApp() {
+  return (
+    <HashRouter>
+      <App />
+    </HashRouter>
+  );
+}
